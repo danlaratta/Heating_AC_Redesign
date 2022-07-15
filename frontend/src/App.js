010b207e248e3ac6a0cfb8eaaf541de68e321cea
Dan <dan.laratta.jr@gmail.com>
2022-07-15 16:47:42 -0400

    testing gh-pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,10 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Heating_AC_Redesign' element={<Home />} />
-        <Route path='/Heating' element={<Heating />} />
-        <Route path='/Air_Conditioning' element={<AirConditioning />} />
-        <Route path='/Services' element={<Services />} />
-        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Heating_AC_Redesign/Heating' element={<Heating />} />
+        <Route path='/Heating_AC_Redesign/Air_Conditioning' element={<AirConditioning />} />
+        <Route path='/Heating_AC_Redesign/Services' element={<Services />} />
+        <Route path='/Heating_AC_Redesign/Contact' element={<Contact />} />
       </Routes>
 
       <Footer />
